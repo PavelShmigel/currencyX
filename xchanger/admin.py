ce3e684f84b613108d63378debaa678af4638b73
Pavel Shmigel <shpashai@gmail.com>
2015-06-09 17:19:44 +0300

    add models to admin

--- xchanger/admin.py
+++ xchanger/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from xchanger.models import Currency, Rates, UpdateInfo
 
-# Register your models here.
+
+class CurrencyAdmin(admin.ModelAdmin):
+    pass
+
+class UpdAdmin(admin.ModelAdmin):
+    pass
+
+
+class RatesAdmin(admin.ModelAdmin):
+    list_filter = ['c_code_id', 'upd_id']
+
+admin.site.register(Currency, CurrencyAdmin)
+admin.site.register(UpdateInfo, UpdAdmin)
+admin.site.register(Rates, RatesAdmin)
